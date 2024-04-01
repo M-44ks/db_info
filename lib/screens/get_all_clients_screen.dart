5db8c340661e2221960763724f6072bc2ba5844e
M-44ks <1maksymadamiuk1@gmail.com>
2024-04-01 14:37:58 +0200

    Package layout and init of first screens

---
+++ lib/screens/get_all_clients_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetAllClientsScreen extends StatefulWidget {
+  const GetAllClientsScreen({super.key});
+
+  @override
+  State<GetAllClientsScreen> createState() => _GetAllClientsScreenState();
+}
+
+class _GetAllClientsScreenState extends State<GetAllClientsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
