38e48f2b7b9e7f305cdf4714b14edb1de537f441
M-44ks <1maksymadamiuk1@gmail.com>
2024-04-12 00:28:25 +0200

    Adding onTap functionality to Clients ListTile

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:db_info/screens/create_client_screen.dart';
+import 'package:db_info/screens/get_clients_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,22 +13,33 @@ class HomeScreen extends StatelessWidget {
       ),
       drawer: Drawer(
         child: ListView(
-          children:  [
+          children: [
             const DrawerHeader(
               decoration: BoxDecoration(color: Colors.blue),
               child: Text('Features'),
             ),
-            ListTile(title: const Text('Create Client'),
-            onTap:() {
-              Navigator.pop(context);
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const CreateClientScreen(),
-                ),
-              );
-            }, ),
-            const ListTile(title: Text('Clients')),
-
+            ListTile(
+              title: const Text('Create Client'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const CreateClientScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Clients'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const GetClientsScreen(),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
