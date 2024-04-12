797ddae6f2666b8b0a2f5648bdd54bebd7669835
M-44ks <1maksymadamiuk1@gmail.com>
2024-04-12 20:58:54 +0200

    Adding capacity field to yacht class

--- lib/models/yacht.dart
+++ lib/models/yacht.dart
@@ -2,11 +2,13 @@ import 'package:db_info/models/pricing.dart';
 
 class Yacht {
   final String name;
+  final int capacity;
   final Pricing highPriceList;
   final Pricing lowPriceList;
 
   const Yacht({
     required this.name,
+    required this.capacity,
     required this.highPriceList,
     required this.lowPriceList,
   });
@@ -14,6 +16,7 @@ class Yacht {
   factory Yacht.fromJson(Map<String, Object?> json) {
     return Yacht(
       name: json['name'] as String? ?? '',
+      capacity: json['capacity'] as int,
       highPriceList: Pricing.fromJson(json['highPriceList'] as Map<String, Object?>? ?? {}),
       lowPriceList: Pricing.fromJson(json['lowPriceList'] as Map<String, Object?>? ?? {}),
     );
@@ -21,6 +24,7 @@ class Yacht {
 
   Map<String, Object?> toJson() => {
     'name': name,
+    'capacity': capacity,
     'highPriceList': highPriceList.toJson(),
     'lowPriceList': lowPriceList.toJson(),
   };
