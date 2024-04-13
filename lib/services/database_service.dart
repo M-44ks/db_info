5f02ef0a218245874029e9ace2f77ec4032f267f
M-44ks <1maksymadamiuk1@gmail.com>
2024-04-13 19:40:26 +0200

    Add yacht operations to DatabaseService
    The changes include adding a new CollectionReference for yachts in the DatabaseService class. This allows the application to fetch yacht information, add a new yacht, update a certain yacht, and delete it. This change will help cater to the yacht-based functionalities in the application.

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,20 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:db_info/models/yacht.dart';
 import '../models/reservation.dart';
 
-
 const String reservationCollectionRef = "reservations";
+const String yachtCollectionRef = "yachts";
 
 class DatabaseService {
   final _firestore = FirebaseFirestore.instance;
 
   late final CollectionReference _reservationsRef;
+  late final CollectionReference _yachtsRef;
 
   DatabaseService() {
-    _reservationsRef = _firestore.collection(reservationCollectionRef).withConverter<Reservation>(
-        fromFirestore: (snapshots, _) => Reservation.fromJson(
-          snapshots.data()!,
-        ),
-        toFirestore: (reservation, _) => reservation.toJson());
+    _reservationsRef = _firestore
+        .collection(reservationCollectionRef)
+        .withConverter<Reservation>(
+            fromFirestore: (snapshots, _) => Reservation.fromJson(
+                  snapshots.data()!,
+                ),
+            toFirestore: (reservation, _) => reservation.toJson());
+
+    _yachtsRef = _firestore.collection(yachtCollectionRef).withConverter<Yacht>(
+        fromFirestore: (snapshots, _) => Yacht.fromJson(
+              snapshots.data()!,
+            ),
+        toFirestore: (yacht, _) => yacht.toJson());
   }
 
   Stream<QuerySnapshot> getReservations() {
@@ -31,5 +41,24 @@ class DatabaseService {
 
   void deleteReservation(String reservationId) {
     _reservationsRef.doc(reservationId).delete();
+  }
+
+ Stream<QuerySnapshot<Object?>> getYachts(capacity) {
+    return _yachtsRef
+        .where('capacity', isGreaterThanOrEqualTo: capacity)
+        .snapshots();
+  }
+
+
+  void addYacht(Yacht yacht) async {
+    _yachtsRef.add(yacht);
+  }
+
+  void updateYacht(String yachtId, Reservation yacht) {
+    _yachtsRef.doc(yachtId).update(yacht.toJson());
+  }
+
+  void deleteYacht(String yachtId) {
+    _yachtsRef.doc(yachtId).delete();
   }
 }
